e63925e0c0e14714d75b35749c3b8ae28ae15578
dderoos <dirk.deroos@gmail.com>
2014-02-18 03:30:43 -0500

    Update install-test-error1.sh

--- install-test-error1.sh
+++ install-test-error1.sh
@@ -28,13 +28,13 @@ sed -i 's/# End of file/biadmin hard nofile 65536\nbiadmin soft nofile 65536\nbi
 sed -i '$a # Set ranges for BigInsights Install\n\nkernel.pid_max = 4194303\nnet.ipv4.ip_local_port_range = 1024 64000' /etc/sysctl.conf
 
 ## turn off ipv6
-#echo '# Turn off IPv6 for BigInsights Install' >> /etc/modprobe.conf
-#echo 'alias net-pf-10 off' >> /etc/modprobe.conf
-#echo 'alias ipv6 off' >> /etc/modprobe.conf
-#sed -i '$a net.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
-#sed -i '$a IPV6INIT=no' /etc/sysconfig/network-scripts/ifcfg-eth0
-#service ip6tables stop
-#chkconfig ip6tables off
+echo '# Turn off IPv6 for BigInsights Install' >> /etc/modprobe.conf
+echo 'alias net-pf-10 off' >> /etc/modprobe.conf
+echo 'alias ipv6 off' >> /etc/modprobe.conf
+sed -i '$a net.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
+sed -i '$a IPV6INIT=no' /etc/sysconfig/network-scripts/ifcfg-eth0
+service ip6tables stop
+chkconfig ip6tables off
 /sbin/reboot
 
 ## start or restart time service
